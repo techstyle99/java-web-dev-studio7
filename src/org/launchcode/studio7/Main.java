d10fd0a1fcba5f23317ba4339cc093d832dd6fad
akyeshi <akyeshi@gmail.com>
2021-10-14 18:49:56 -0400

    just testing

--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args){
 
+        // excellent!
         // TODO: Declare and initialize a CD and a DVD object.
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
